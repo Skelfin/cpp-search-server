758c0385be1e4d44cfcb57af68384f59c844fa61
Danil Biryukov <danilbiryukov04@gmail.com>
2024-04-08 00:22:51 +0300

    Finished

--- search-server/request_queue.cpp
+++ search-server/request_queue.cpp
@@ -23,3 +23,22 @@ void RequestQueue::AddRequest(int results_num) {
         ++no_results_requests_;
     }
 }
+
+template <typename DocumentPredicate>
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+    const auto result = search_server_.FindTopDocuments(raw_query, document_predicate);
+    AddRequest(result.size());
+    return result;
+}
+
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentStatus status) {
+    const auto result = search_server_.FindTopDocuments(raw_query, status);
+    AddRequest(result.size());
+    return result;
+}
+
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query) {
+    const auto result = search_server_.FindTopDocuments(raw_query);
+    AddRequest(result.size());
+    return result;
+}
